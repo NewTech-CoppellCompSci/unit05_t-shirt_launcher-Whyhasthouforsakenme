614d55f9cb65651c93992ad4ceab22629e35f3a9
Mikaela Amari Omaoeng <momaoeng@newtech.coppellisd.com>
2023-01-05 12:06:48 -0600

    Progress in lab 2

--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -13,15 +13,35 @@ public class Lab02 {
 
 	}
 
-
+	// Asks the user for two positive integers. Then prints 100 random numbers between them.
 	public static void problem1() {
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.print("Enter a positive integer: ");
+		int firstInt = inKey.nextInt();
+		System.out.print("Enter another positive integer: ");
+		int secInt = inKey.nextInt();
 		
+		int max = Math.max(firstInt, secInt);
+		int min = Math.min(firstInt, secInt);
+		
+		for (int i = 0; i < 100; i++) {
+			System.out.print(((int)(Math.random() * (max - min + 1)) + min) + " ");
+		}
 	}
 
-
+	// Asks the user for the radius and height of a cylinder. Then prints out the volume.
 	public static void problem2() {
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.println("\n\nEnter the cylinder's radius: ");
+		double radius = inKey.nextDouble();
+		System.out.println("Enter the cylinder's height: ");
+		double height = inKey.nextDouble();
+		
+		double cylVol =  Math.PI * Math.pow(radius, 2) * height;
+		
+		System.out.print("Enter the cylinder's volume: " + cylVol);
 		
 	}
 
