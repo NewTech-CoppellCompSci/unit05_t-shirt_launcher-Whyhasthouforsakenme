e7a1100c7f7d6eaa249b01350a35a1aa539291bf
Mikaela Amari Omaoeng <momaoeng@newtech.coppellisd.com>
2023-01-11 11:56:27 -0600

    Finished performance task but it saved again for some reason

--- src/performance_task/Performance_Task.java
+++ src/performance_task/Performance_Task.java
@@ -14,15 +14,19 @@ public class Performance_Task {
 		System.out.println("Enter launch angle (degrees) >>> ");
 		double angle = inKey.nextDouble();
 		
+		// Beginning of the projectile's path stats.
 		System.out.println("\nProjectile's path: \n");
 		
+		// Variables for time, x-position, and y-position.
 		int time = 0;
 		double x = 0;
 		double y = 0;
 		
+		// Prints the stats (time, location, etc) in a loop until y-pos is zero.
 		do {
 			System.out.println("Time: " + time + "s");
 			
+			// Calculates the x-pos and y-pos.
 			x = Math.cos(Math.toRadians(angle)) * time * velocity;
 			y = Math.sin(Math.toRadians(angle)) * time * velocity - 0.5 * 9.8 * Math.pow(time, 2);
 			
@@ -33,26 +37,16 @@ public class Performance_Task {
 			System.out.println("\tx-pos: " + x + "m");
 			System.out.println("\ty-pos: " + y + "m");
 			
+			// Increments the time.
 			time++;
 			
 				
 		} while (y >= 0);
 		
+		// Prints the final x-pos when y-pos is zero.
 		System.out.println("\tx-pos: " + x + "m");
 		System.out.println("\ty-pos: 0m");
 		
-		
-		/** for (int i = 0; i < 3; i++) {
-			System.out.println("Time: " + time + "s");
-			
-			x = Math.cos(Math.toRadians(angle)) * time * velocity;
-			System.out.println("\tx-pos: " + x + "m");
-			y = Math.sin(Math.toRadians(angle)) * time * velocity - 0.5 * 9.8 * Math.pow(time, 2);
-			System.out.println("\ty-pos: " + y + "m");
-			
-			time++;
-		}
-		**/
 	}
 	
 }
